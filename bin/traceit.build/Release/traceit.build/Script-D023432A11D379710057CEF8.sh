73997908430eccdd09421b0878566ce6601e3bc7
Jeroen Bourgois <jeroenbourgois@gmail.com>
2010-06-24 13:42:24 +0200

    xcode update

--- bin/traceit.build/Release/traceit.build/Script-D023432A11D379710057CEF8.sh
+++ bin/traceit.build/Release/traceit.build/Script-D023432A11D379710057CEF8.sh
@@ -4,12 +4,12 @@ set -ex
 [ "$ACTION" = build ] || exit 0
 [ "$BUILD_VARIANTS" = "normal" ] || exit 0
 dir="$BUILD_DIR/$CONFIGURATION/"
-dmg="$BUILD_DIR/$CONFIGURATION/$EFFECTIVE_PLATFORM_NAME.dmg"
+dmg="$EFFECTIVE_PLATFORM_NAME.dmg"
 
 rm -rf "$dir"
 mkdir "$dir"
-cp -R "$BUILD_DIR/$CONFIGURATION/$EFFECTIVE_PLATFORM_NAME.app" "$dir"
+cp -R "$BUILD_DIR/$CONFIGURATION/$PROJECT_NAME.app" "$dir"
 rm -f "$dmg"
-hdiutil create -srcfolder "$dir" -volname "$EFFECTIVE_PLATFORM_NAME" "$dmg"
+hdiutil create -srcfolder "$dir" -volname "$PROJECT_NAME" "$dmg"
 hdiutil internet-enable -yes "$dmg"
 rm -rf "$dir"
